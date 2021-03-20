98f559483e6ea3314d308ffda3feeb9590121a73
gh0stadian <y5F2RNymnkBJCT>
2021-03-20 13:24:06 +0100

    init flutter app

---
+++ lib/matches.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+
+class MatchesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
